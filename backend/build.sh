553f1929fa145f44e1084aaf31bf683096a26d77
Boray Öztürk <boraybiology@gmail.com>
2025-10-23 21:58:48 +0300

    Update psycopg2 and build script

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,8 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
+# Collect static files (without database)
+DJANGO_SETTINGS_MODULE=petkey_api.settings python manage.py collectstatic --no-input --noinput
 
 # Run migrations
 python manage.py migrate
